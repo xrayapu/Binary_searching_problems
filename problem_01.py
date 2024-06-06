9f752ed1baa09a8eebc010b0c415c1598353fabc
apu <40669057+xrayapu@users.noreply.github.com>
2024-06-06 20:50:31 +0600

    Update problem_01.py

--- problem_01.py
+++ problem_01.py
@@ -1,7 +1,7 @@
 # binary search normal implementation
 def sol(arr, x):
     lo=0
-    hi=len(arr)
+    hi=len(arr)-1
     while lo<= hi:
         mid=lo+ (hi-lo)//2 # mid = (lo +hi )//2 is not used because it gives overflow problems, so it's safe in this way.
         if arr[mid] == x:
