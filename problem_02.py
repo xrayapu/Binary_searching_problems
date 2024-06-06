d0c8bcdc91c4e88fdee54a85500729b7446f8aa2
apu <40669057+xrayapu@users.noreply.github.com>
2024-06-06 19:55:42 +0600

    Create problem_02.py

---
+++ problem_02.py
@@ -0,0 +1,18 @@
+# search an element in reversed array.
+
+def sol(arr, x):
+    lo=0
+    hi=len(arr)
+    while lo<= hi:
+        mid=lo+ (hi-lo)//2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] > x:
+            lo = mid+1
+
+        else:
+          hi = mid -1 
+
+    return -1
+
+#print(sol([11,10,9,7,4,3,2,1],1)) # output:7
