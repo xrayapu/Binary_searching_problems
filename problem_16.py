c77a16f77873f0510255a5ae09571bf32829c6b5
apu <40669057+xrayapu@users.noreply.github.com>
2024-06-21 19:24:50 +0600

    Update and rename problem_16.py to problem_17.py

--- problem_16.py
+++
@@ -1,4 +0,0 @@
-#problem 16: search a element in biotonic array. 
-# biotonic means: stright increasing , get to a pick element , then stright decreasing .
-
-# 1,3,5,6,7,4,3,1 -> biotonic . 1,3,4,5,6,7,7,5,3,2,1 -> not biotonic 
---
+++ problem_17.py
@@ -0,0 +1,4 @@
+#problem 17: search a element in bitonic array. 
+# biotonic means: stright increasing , get to a pick element , then stright decreasing .
+
+# 1,3,5,6,7,4,3,1 -> bitonic . 1,3,4,5,6,7,7,5,3,2,1 -> not bitonic 
